8f5abac3d95cc46de6f9931f109328dd601c4d1e
SamuelSchutz13 <samuelschutz14@gmail.com>
2023-10-25 20:23:04 -0300

    add functions in struct code

--- main.cpp
+++ main.cpp
@@ -1,37 +1,72 @@
-#include <stdio.h>
-#include <locale.h>
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
-void menu() {
-    puts("1 - Inserir número");
-    puts("2 - ");
-    puts("3 - ");
-    puts("4 - ");
-    puts("5 - Sair");
+struct Number {
+    int value;
+    Number (int val) : value(val) {}
+};
+
+void insert_element(std::vector<Number> &array) {
+    int number;
+    std::cout << "Digite o numero a ser inserido: ";
+    std::cin >> number;
+    array.push_back(Number(number));
+    std::cout << "Numero" << number << "foi inserido com sucesso" << std::endl;
 }
 
-void process_menu() {
-    int command;
-    do {
-        scanf("%d", &command);
-        switch (command) {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            default: puts("Comando inválido, tente novamente");
-        }
-    } while (command != 5);
+void insertion_sort() {
+
+}
+
+void merge_sort() {
+
+}
+
+void quick_sort() {
+
+}
+
+void menu() {
+    std::cout << "1 - Inserir elemento" << std::endl;
+    std::cout << "2 - Ordenar Insertion Sort" << std::endl;
+    std::cout << "3 - Ordenar Merge Sort" << std::endl;
+    std::cout << "4 - Ordenar Quick Sort" << std::endl;
+    std::cout << "5 - Sair" << std::endl;
+    std::cout << "Escolha uma opcao: " << std::endl;
+}
+
+void process_menu(std::vector<Number> &array, int command) {
+    switch (command) {
+        case 1:
+            insert_element(array);   
+            break;
+        case 2:
+            insertion_sort();
+            break;
+        case 3:
+            merge_sort();
+            break;
+        case 4:
+            quick_sort();
+            break;
+        case 5:
+        std::cout << "Saindo do sistema..." << std::endl;
+        exit(1);
+            break;
+        default: std::cout << "Comando invalido, tente novamente" << std::endl;
+    }
 }
 
 int main() {
-    menu();
-    process_menu();
+    int command;
+    std::vector<Number> array;
+    
+    do {
+        menu();
+        scanf("%d", &command);
+        //process_menu(command);
+    } while (command != 5);
+
     return 0;
 }
